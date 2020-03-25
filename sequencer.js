82ae7e94c38cbb4bee73d117b6b1b15e6ef24b09
NickOfRhyme <nhwoolrich@gmail.com>
2020-03-25 22:19:24 +0000

    four beat kick drum with play/pause setup

--- sequencer.js
+++ sequencer.js
@@ -1,2 +1,56 @@
-let BPM = 120;
-function advanceBeat() {}
+let currBeatNum = 0;
+let beatInterval;
+const totalBeats = 4;
+const playPauseButton = document.querySelector("button.play");
+
+playPauseButton.addEventListener("click", playPauseToggle);
+
+function startBeat(BPM = 100) {
+  beatInterval = setInterval(advanceBeat, (60 / BPM) * 1000);
+}
+
+function advanceBeat() {
+  const lastBeatDisplay = document.querySelector(`.beat${currBeatNum}`);
+  lastBeatDisplay && lastBeatDisplay.classList.remove("active");
+
+  currBeatNum === totalBeats ? (currBeatNum = 1) : currBeatNum++;
+
+  const currBeatDisplay = document.querySelector(`.beat${currBeatNum}`);
+
+  currBeatDisplay.classList.add("active");
+
+  console.log(currBeatDisplay);
+
+  playSound(currBeatDisplay.dataset.drum);
+}
+
+function pauseBeat() {
+  clearInterval(beatInterval);
+}
+
+function stopBeat() {
+  pauseBeat();
+  const currBeatDisplay = document.querySelector(`.beat${currBeatNum}`);
+  currBeatDisplay.classList.remove("active");
+  currBeatNum = 0;
+}
+
+function playSound(drumType) {
+  console.log(drumType);
+  const sound = document.querySelector(`#${drumType}`);
+  sound.pause();
+  sound.currentTime = 0;
+  sound.play();
+}
+
+function playPauseToggle({ target: { classList } }) {
+  if (classList.contains("play")) {
+    classList.remove("play");
+    classList.add("pause");
+    startBeat();
+  } else {
+    classList.remove("pause");
+    classList.add("play");
+    stopBeat();
+  }
+}
